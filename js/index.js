901646f555a3428188266a6380f50b60fbf3ff93
VeronikaKoma <komar2002@gmail.com>
2022-11-20 01:51:05 +0100

    added change peliculas.json

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const requestURL = '../json/peliculas.json';
+
+async function fetchMoviesJson(){
+    const response = await fetch (requestURL);
+    const movies = await response.json();
+    return movies;
+}
+
+    fetchMoviesJson().then(movies => {
+    for(let index = 0 ; index < movies.peliculas.lenght ; index++){
+    
+    console.log(movies.peliculas[index].nombre);
+
+    };
+    
+})
+
+
