9009ba33eadd48093714b8cb94f071b211766ce1
VeronikaKoma <komar2002@gmail.com>
2022-11-20 20:24:56 +0100

    add changes index.html peliculas.json index.css index.js

--- js/index.js
+++ js/index.js
@@ -5,11 +5,33 @@ async function fetchMoviesJson(){
     const movies = await response.json();
     return movies;
 }
-    fetchMoviesJson().then(movies =>{
-    for(let index = 0 ; index < movies.peliculas.length; index++){
-    
-    console.log(movies.peliculas[index].titulo);
 
-    };
+
+    fetchMoviesJson().then(movies =>{
+    for(let index = 0 ; index < movies.peliculas.length; index++)
+    {
+    const moviesSection = document.getElementById('movieSection');
+
+        let id = movies.peliculas[index].id;
+        
+        let title = movies.peliculas[index].titulo;
+        let descripcion = movies.peliculas[index].descripcion;
+        let director = movies.peliculas[index].director;
+        let genre = movies.peliculas[index].clasification;
+        let img = movies.peliculas[index].caratula;
     
+        //console.log(movies.peliculas[index].id);
+
+        moviesSection.innerHTML += `
+            <div class="card" style="width: 18rem;">
+                <img src="${img}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h3 class="card-title">${id}. ${title}</h3>
+                    <h5 class="card-title">Director : ${director}</h5>
+                    <h5 class="card-title">Genero : ${genre}</h5>
+                </div>
+            </div>
+            `
+        
+    };
 })
